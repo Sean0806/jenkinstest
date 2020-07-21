dbfdefc73b5ca02debdcdfbb5ae13430156f2eb1
沈聪 <cshen@abcft.com>
2020-07-21 09:50:01 +0800

    init project

---
+++ src/main/java/com/sean/own/jenkinstest/controller/TestController.java
@@ -0,0 +1,20 @@
+package com.sean.own.jenkinstest.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author 沈聪
+ * @Description
+ * @create 2020-07-21 9:44
+ */
+@RestController
+@RequestMapping("/testController")
+public class TestController {
+
+    @GetMapping("/testApi")
+    public void testApi(){
+        System.out.println("成功进入接口");
+    }
+}
